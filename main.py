ce8994aff2456e2863f92feba9acda78fcc04424
profluizdias <luizdias01@prof.educacao.sp.gov.br>
2026-05-04 12:12:31 -0300

    adiciona o menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+while True:
+    print('Totem de Autoatendimento de Cafeteria/Lanchonete')
+    
+    print('1. Produtos')
+    print('2. Carrinho')
+    print('3. ')
+    print('4. Sair')
+
+    opcao = input('Escolha a opção desejada: ')
